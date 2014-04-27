b08acb802dfc716c9c32294bfcad3901c63acc7e
Agathe Gilain <agathe.gilain@telecom-paristech.fr>
2014-04-27 12:49:15 +0200

    Mod

--- src/MainPhoto.java
+++ src/MainPhoto.java
@@ -14,35 +14,14 @@ public class MainPhoto {
 	 */
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
-		
-		//BufferedImage newImage = ImageIO.read(new File("data/malatest.png"));
-		//Color c = new Color(newImage.getRGB(newImage.getHeight()/2, newImage.getWidth()/2));
-		//System.out.println(newImage.getHeight());
-		//System.out.println(newImage.getWidth());
-
-		//System.out.println(c.getRGB());
-		//System.out.println(c.getGreen());
-		
+				
 		Plante p = new Plante(null);
-		//if(args.length>0)
-			//p.setPlante3(args[0]);
-		//else
+		if(args.length>0)
+			p.setPlante3(args[0]);
+		else
 			p.setPlante3("pourcen/21.04m");
 		p.utkppv(6);
-		//p.getSante();
-		//p.imagedResult2("ts.png");
-		//p.rectangle("data/big.png");
 		
-	
-		//p.setPlante32("Test3/21.png", "Test3/23.png");
-		//p.getSante2();
-		//p.imagedResult22("Test3/21.png", "Test3/23.png");
-		
-		//Pixel p = new Pixel(100000,10000, 19999, 1000000000) ;
-		//p.kppvPixel(6);
-		
-
 	}
 
 }
